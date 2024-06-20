025671e975eae0522d911c5dd409ccda11965aa1
Lyamkin Sergey <lyamkin.sergey.alex@gmail.com>
2024-06-20 23:59:42 +0300

    add hw2 additional and remake final work

--- my_app/app.py
+++ my_app/app.py
@@ -1,15 +1,16 @@
-from flask import Flask
+from flask import Flask, request
 import logging
 
 app = Flask(__name__)
 
-# Настройка логирования
+# Настроим логирование
 logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 @app.route('/')
-def home():
-    app.logger.info("Home page accessed")
-    return "Hello, World!"
+def hello_world():
+    logger.info('Hello world endpoint was reached')
+    return 'Hello, World!'
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0')
